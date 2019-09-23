2ea5849edc67b9a6086cc3992224f37c48332b0c
Nora Liu <tzuyuanliu0830@gmail.com>
2019-09-23 21:56:57 +0800

    add og image

--- pages/_document.js
+++ pages/_document.js
@@ -1,33 +1,40 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import styledNormalize from 'styled-normalize'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Main, NextScript } from 'next/document';
+import styledNormalize from 'styled-normalize';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
-    const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props => {
-      return sheet.collectStyles(<app {...props}/>);
-    });
-    const styleTags = sheet.getStyleElement();
-    return {
-      ...page,
-      styleTags
-    };
-  }
+	static getInitialProps({ renderPage }) {
+		const sheet = new ServerStyleSheet();
+		const page = renderPage((App) => (props) => {
+			return sheet.collectStyles(<app {...props} />);
+		});
+		const styleTags = sheet.getStyleElement();
+		return {
+			...page,
+			styleTags
+		};
+	}
 
-  render() {
-    return (
-      <html>
-      <Head>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
-        <meta charSet="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1,shrink-to-fit=no"/>
-        <meta name="description" content="WizrdAmigos Codecamp will held a monthly “FREE” Boot Camp!!!" />
-        <meta property="og:title" content="WizardAmigos Codecamp 2018" />
-        <meta property="og:image" content="https://wizardamigos-codecamp2018.herokuapp.com/static/og_image.jpg" />
-        <meta property="og:url" content="https://wizardamigos-codecamp2018.herokuapp.com/landingPage" />
-        <link rel="icon" type="image/x-icon" href="static/favicon.ico" />
-        <style>{`
+	render() {
+		return (
+			<html>
+				<Head>
+					<meta httpEquiv="X-UA-Compatible" content="IE=edge, chrome=1" />
+					<meta charSet="utf-8" />
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1, maximum-scale=1,shrink-to-fit=no"
+					/>
+					<meta name="description" content="WizrdAmigos Codecamp will held a monthly “FREE” Boot Camp!!!" />
+					<meta property="og:title" content="WizardAmigos Codecamp 2019" />
+					<meta
+						property="og:image"
+						content="https://wizardamigos-codecamp2019.herokuapp.com/static/og_image.png"
+					/>
+					<meta property="og:url" content="https://wizardamigos-codecamp2019.herokuapp.com/" />
+					<link rel="icon" type="image/x-icon" href="static/favicon.ico" />
+					<style>
+						{`
             ${styledNormalize}
             html, body{
               font-family:Helvetica Neue, Helvetica, Arial, PingFang TC, 微软雅黑, Microsoft YaHei, 华文细黑, STHeiti, sans-serif;
@@ -37,15 +44,15 @@ export default class MyDocument extends Document {
               color: #fff;
             }
           `}
-        </style>
-        
-        {this.props.styleTags}
-      </Head>
-      <body>
-      <Main />
-      <NextScript/>
-      </body>
-      </html>
-    )
-  }
+					</style>
+
+					{this.props.styleTags}
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</html>
+		);
+	}
 }
